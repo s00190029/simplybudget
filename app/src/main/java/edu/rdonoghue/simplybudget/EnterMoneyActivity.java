2341081426f8bdda23b484303f469aff6bf05226
sviney <sviney@users.noreply.github.com>
2020-12-01 17:54:59 +0000

    links added and fixed a textview

--- app/src/main/java/edu/rdonoghue/simplybudget/EnterMoneyActivity.java
+++ app/src/main/java/edu/rdonoghue/simplybudget/EnterMoneyActivity.java
@@ -3,6 +3,7 @@ package edu.rdonoghue.simplybudget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class EnterMoneyActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class EnterMoneyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_money);
+    }
+
+    public void DoMoneyCatBack(View view) {
+        finish();
     }
 }
--- app/src/main/java/edu/rdonoghue/simplybudget/StartLumpSumActivity.java
+++ app/src/main/java/edu/rdonoghue/simplybudget/StartLumpSumActivity.java
@@ -3,6 +3,7 @@ package edu.rdonoghue.simplybudget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class StartLumpSumActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class StartLumpSumActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_lump_sum);
+    }
+
+    public void DoBack(View view) {
+        finish();
     }
 }
