b76c270b7b9d6eca98a0b52aa58921cede582f09
s00189168 <anna.gwizdz@mail.itsligo.ie>
2020-12-02 14:44:48 +0000

    display cash button set to a availableCash variable

--- app/src/main/java/edu/rdonoghue/simplybudget/MainActivity.java
+++ app/src/main/java/edu/rdonoghue/simplybudget/MainActivity.java
@@ -5,14 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
-    public static float availableCash;
+    public static float availableCash = 0;
+    Button btnAvailableCash;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnAvailableCash = findViewById(R.id.btnCashDisplay);
+        btnAvailableCash.setText("Cash: €" + String.valueOf(availableCash));
+
     }
 
     public static void setCash(float amtIn){
