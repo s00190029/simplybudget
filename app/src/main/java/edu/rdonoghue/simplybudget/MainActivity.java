9eb88cfd959834cb3a5c7164f3fd39fbc8622ec8
s00190029 <mrdaylight@protonmail.com>
2020-12-04 23:30:08 +0000

    singular spendInvest page, euros update, 1st starter cat works

--- app/src/main/java/edu/rdonoghue/simplybudget/MainActivity.java
+++ app/src/main/java/edu/rdonoghue/simplybudget/MainActivity.java
@@ -14,8 +14,10 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     public static Button btnAvailableCash;
-    public static float availableCash = 0;
-    TextView tvCatCash1, tvCatCash2, tvCatName1, tvCatName2;
+    public static float availableCash;
+    public static TextView tvCatCash1, tvCatCash2, tvCatName1, tvCatName2;
+    public Button catButton;
+    public static category starter1, starter2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,17 +31,30 @@ public class MainActivity extends AppCompatActivity {
         tvCatCash1 = findViewById(R.id.tvCatCash);
         tvCatName2 = findViewById(R.id.tvCatName2);
         tvCatCash2 = findViewById(R.id.tvCatCash2);
-        category starter1, starter2;
         starter1 = new category("Groceries", 100.0f);
         starter2 = new category("Bills");
         tvCatName1.setText(starter1.name);
         tvCatCash1.setText(String.valueOf(starter1.balance));
         tvCatName2.setText(starter2.name);
         tvCatCash2.setText(String.valueOf(starter2.balance));
+
+        /*  Button listeners
+        *   Category long press
+        */
+        catButton = findViewById(R.id.btnCat);
+        catButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Intent expandedCatView = new Intent(view.getContext(), ExpandedCategoryActivity.class);
+                startActivity(expandedCatView);
+                return true;
+            }
+        });
     }
 
     public static void setCash(float amtIn){
         availableCash = amtIn;
+        btnAvailableCash.setText(String.valueOf(amtIn));
     }
 
     public static void updateCash(float amtIn, boolean plusOrMinus){
@@ -49,16 +64,19 @@ public class MainActivity extends AppCompatActivity {
         else {
             availableCash -= amtIn;
         }
+        btnAvailableCash.setText(String.valueOf(availableCash));
     }
 
-    public void doExpandedCat(View view) {
-        Intent ExpandedCat = new Intent(view.getContext(),ExpandedCategoryActivity.class);
-        startActivity(ExpandedCat);
-
+    public void doSpendInvestCat(View view) {
+        Intent investSpendCat1 = new Intent(view.getContext(), StartLumpSumActivity.class);
+        //getIntent().putExtra("catID", starter1.id);
+        investSpendCat1.putExtra("walletType", 1);
+        startActivity(investSpendCat1);
     }
 
     public void DoLumSumInput(View view) {
         Intent LumpSum = new Intent(view.getContext(),StartLumpSumActivity.class);
+        LumpSum.putExtra("walletType", 0);
         startActivity(LumpSum);
     }
 }
--- app/src/main/java/edu/rdonoghue/simplybudget/StartLumpSumActivity.java
+++ app/src/main/java/edu/rdonoghue/simplybudget/StartLumpSumActivity.java
@@ -2,32 +2,49 @@ package edu.rdonoghue.simplybudget;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 public class StartLumpSumActivity extends AppCompatActivity {
 EditText eTcashValue;
-public Float cashValue;
+public Float userInput;
+public int walletType;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_lump_sum);
         eTcashValue =findViewById(R.id.etLumpSumInput);
-
+        walletType = getIntent().getIntExtra("walletType", 0);
     }
 
     public void DoBack(View view) {
         finish();
     }
 
-    public void doSave(View view) {
-        //float floatValueOf;
-        cashValue = Float.valueOf(eTcashValue.getText().toString());
-        MainActivity.setCash(cashValue);
-        Intent mainAct = new Intent(view.getContext(),MainActivity.class);
-        startActivity(mainAct);
+    public void doInvest(View view) {
+        if (walletType == 0){
+            userInput = Float.valueOf(eTcashValue.getText().toString());
+            MainActivity.updateCash(userInput, true);
+        }
+        else if (walletType == 1){
+            userInput = Float.valueOf(eTcashValue.getText().toString());
+            MainActivity.starter1.updateBalance(userInput, true);
+        }
+        finish();
+    }
+
+    public void doSpend(View view) {
+        walletType = getIntent().getIntExtra("walletType", 0);
+        float userInput = Float.valueOf(eTcashValue.getText().toString());
+        if (walletType == 1) {
+            MainActivity.starter1.updateBalance(Float.valueOf(eTcashValue.getText().toString()), false);
+            MainActivity.tvCatCash1.setText(String.valueOf(MainActivity.starter1.balance));
+            //MainActivity.updateCash(userInput, false);
+        }
+        else if (walletType == 0) {
+            MainActivity.updateCash(userInput, false);
+        }
+        finish();
     }
 }
