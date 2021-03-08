ea93e79a4fda82b182a2663e4c3e888b067c9acf
s00190029 <daylight@manjaro.station>
2021-03-08 21:29:05 +0000

    expanded feature working on Rob machine

--- app/src/main/java/edu/rdonoghue/simplybudget/ExpandedCategoryActivity.java
+++ app/src/main/java/edu/rdonoghue/simplybudget/ExpandedCategoryActivity.java
@@ -9,16 +9,29 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+
 
 public class ExpandedCategoryActivity extends AppCompatActivity {
+
+    public int catID;
+    public EditText nameField, moneyInput;
+    private static MySQLiteHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expanded_category);
+        dbHelper = new MySQLiteHelper(this);
 
         //for back button on action bar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        catID = getIntent().getIntExtra("catID", 0);
+        nameField = findViewById(R.id.etCatName);
+        nameField.setText(dbHelper.getOneCategory(catID).name);
+        moneyInput = findViewById(R.id.etCatCash);
+        //nameField = findViewById(R.id.)
+
 
     }
 
@@ -44,6 +57,7 @@ public class ExpandedCategoryActivity extends AppCompatActivity {
         if(item.getItemId() == R.id.AddIcon)
         {
             Intent Act= new Intent(this,StartLumpSumActivity.class);
+            Act.putExtra("catID", catID);
             startActivity(Act);
         }
         else
@@ -53,5 +67,18 @@ public class ExpandedCategoryActivity extends AppCompatActivity {
         }
 
         return true;
+    }
+
+    public void expandSave(View view) {
+        if (nameField != null){
+            dbHelper.updateOneCategoryName(catID, nameField.getText().toString());
+        }
+        if (moneyInput != null){
+            float tempMoney = Float.parseFloat(moneyInput.getText().toString());
+            dbHelper.updateOneCategoryBalance(catID, tempMoney);
+        }
+
+        MainActivity.updateVisuals();
+        finish();
     }
 }
